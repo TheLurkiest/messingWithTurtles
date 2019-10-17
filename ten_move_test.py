c43189aabd408aa28bf1da8282d5cbdc36ed29e5
TheLurkiest <bng5@wildcats.unh.edu>
2019-10-17 19:59:41 -0400

    got python code turtles to perform 10 moves

--- ten_move_test.py
+++ ten_move_test.py
@@ -7,12 +7,9 @@ ob1.moveturtle( func[0], func[1] )
 
 # ...and boom!  The turtle moves!!! yay!
 
-
 for elem_r in range(1,11):
 	ob1.moveturtle( func[0], func[1] )
 
-# ob1.moveturtle( func[0], func[1] )
-# ob1.moveturtle( func[0], func[1] )
 
 
 
