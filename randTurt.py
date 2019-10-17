74f4df53540f31047bb34dffa7191a07365b5311
TheLurkiest <bng5@wildcats.unh.edu>
2019-10-17 16:09:30 -0400

    testing out files

--- randTurt.py
+++ randTurt.py
@@ -13,68 +13,68 @@ import random
 #wn = turtle.Screen()
 
 class RandT1(object):
-    """ has methods that do turtle stuff """
+	""" has methods that do turtle stuff """
 
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
-    def turtlesetup(self):
+	def turtlesetup(self):
 
-        jimmy = turtle.Turtle()
-        o = turtle.Turtle()
-        wn = turtle.Screen()
+		jimmy = turtle.Turtle()
+		o = turtle.Turtle()
+		wn = turtle.Screen()
 
-        jimmy.shape('turtle')
-        jimmy.color("red")
-        jimmy.pensize(5)
-        jimmy.forward(2)
+		jimmy.shape('turtle')
+		jimmy.color("red")
+		jimmy.pensize(5)
+		jimmy.forward(2)
 
 
-        starttx = random.randrange(-50,50)
-        startty = random.randrange(-50,50)
+		starttx = random.randrange(-50,50)
+		startty = random.randrange(-50,50)
 
-        jimmy.penup()
-        jimmy.setpos(starttx,startty)
-        jimmy.pendown()
+		jimmy.penup()
+		jimmy.setpos(starttx,startty)
+		jimmy.pendown()
 
-        return(wn,jimmy)
+		return(wn,jimmy)
 
-    def moveturtle(self,wn,jimmy):
-        #while isInScreen(wn, jimmy):
-        angle = random.randrange(1, 361)
-        jimmy.right(angle)
-        jimmy.forward(50)
+	def moveturtle(self,wn,jimmy):
+		#while isInScreen(wn, jimmy):
+		angle = random.randrange(1, 361)
+		jimmy.right(angle)
+		jimmy.forward(50)
 
-    def turtFuncUturn(self, jimmy):
-        """
-        do a u turn with this method
-        """
-        # assuming we're starting facing east
-        # go forward a short distance
-        # ...then turn left 90 degrees
-        # ...then go straight a short distance
+	def turtFuncUturn(self, jimmy):
+		"""
+		do a u turn with this method
+		"""
+		# assuming we're starting facing east
+		# go forward a short distance
+		# ...then turn left 90 degrees
+		# ...then go straight a short distance
 
-        jimmy.forward((random.randint(25,100)))#25-100
+		jimmy.forward((random.randint(25,100)))#25-100
 
-        # ...then turn
-        jimmy.left(90)
-        jimmy.forward((random.randint(25,100)))
+		# ...then turn
+		jimmy.left(90)
+		jimmy.forward((random.randint(25,100)))
 
-        # ...and turn again
-        jimmy.left(90)
-        jimmy.forward((random.randint(25,100)))
+		# ...and turn again
+		jimmy.left(90)
+		jimmy.forward((random.randint(25,100)))
 #=============================================================================
 
-    # after entering:
-    # import randTurt
-    # ...into idle you can create a turtle object and then make it move randomly
-    # in the following way:
-    # enter the following line of code to start the program:
-    # ...after making sure to enter this: ob1 = randTurt.RandT1()
-    # ...then enter this one line:
-    # y = ob1.turtlesetup()
-    # ...or something like this: func = ob1.turtlesetup()
+	# after entering:
+	# import randTurt
+	# ...into idle you can create a turtle object and then make it move randomly
+	# in the following way:
+	# enter the following line of code to start the program:
+	# ...after making sure to enter this: ob1 = randTurt.RandT1()
+	# ...then enter this one line:
+	# y = ob1.turtlesetup()
+	# ...or something like this: func = ob1.turtlesetup()
 
-    # ...followed by something like this:
-    # ob1.moveturtle( func[0], func[1] )
-    # ...and boom!  The turtle moves!!! yay!
+	# ...followed by something like this:
+	# ob1.moveturtle( func[0], func[1] )
+	# ...and boom!  The turtle moves!!! yay!
