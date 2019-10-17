640055f02dd7c0023119cb2eb383de6c0a1e8019
TheLurkiest <bng5@wildcats.unh.edu>
2019-10-17 17:59:39 -0400

    added a functional ten move equivalent of test1

---
+++ ten_move_test.py
@@ -0,0 +1,23 @@
+import randTurt
+
+ob1 = randTurt.RandT1()
+
+func = ob1.turtlesetup()
+ob1.moveturtle( func[0], func[1] )
+
+# ...and boom!  The turtle moves!!! yay!
+
+
+for elem_r in range(1,11):
+	ob1.moveturtle( func[0], func[1] )
+
+# ob1.moveturtle( func[0], func[1] )
+# ob1.moveturtle( func[0], func[1] )
+
+
+
+
+
+
+
+
