1dc2b623a760f2cb8dfd3cfe26371f13b53cecad
TheLurkiest <bng5@wildcats.unh.edu>
2019-10-17 20:54:41 -0400

    added player custom character creation menu of sorts

--- play_as_turtle.py
+++ play_as_turtle.py
@@ -7,16 +7,19 @@ import random
 import unittest
 import pdb
 
-import randTurt
-from randTurt import RandT1
+import playerTurt
+from playerTurt import MeTurt
 
 
-ob1 = randTurt.RandT1()
-func1 = ob1.turtlesetup()
+p_reply=''
+p_reply = input('Enter the COLOR you want your turtle to be: ')
+
+ob1 = playerTurt.MeTurt()
+func1 = ob1.turtlesetup( str(p_reply) )
 
 count1=0
 while(count1 < 5):
-    ob1.moveturtle( func1[0], func1[1] )
-    count1 = count1 + 1
+	ob1.moveturtle( func1[0], func1[1] )
+	count1 = count1 + 1
 
 
